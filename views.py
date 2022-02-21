66ae662fecf5d7b4cee9d21eb28862263066709a
TosinOtuseso <69818335+TosinOtuseso@users.noreply.github.com>
2022-02-21 06:55:49 +0100

    Add files via upload

--- views.py
+++ views.py
@@ -14,9 +14,10 @@ def index (request):
 
 
 def meetup_details(request, meetups_slug): 
-    selected_meetup,created = Meetups.objects.get_or_create(slug = meetups_slug)
+    selected_meetup,created = Meetups.objects.get(slug = meetups_slug)
     
     return render(request, 'meetups/meetup_details.html', {
     'meetup-title':selected_meetup.title ,
     'meetup-description':selected_meetup.Description })
+    print (created)
     
